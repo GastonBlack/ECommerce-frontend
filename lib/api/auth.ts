1b2126a1939c45426f8c70fdb023682aea37e28e
GastonBlack <gaston.galeano.r@gmail.com>
2026-01-09 20:57:49 -0300

    Ahora si te registras te mantenes logueado, asi el usuario no tiene que registrarse y despues loguear otra vez.

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -3,29 +3,30 @@ import api from "./axios";
 export const authService = {
     async login(email: string, password: string) {
         const res = await api.post("/auth/login", { email, password });
-        const { token, userId, email: userEmail } = res.data;
 
-        //  Guardar token
+        const { token, userId, email: userEmail, fullName } = res.data;
+
+        // Guardar sesion
         localStorage.setItem("token", token);
+        localStorage.setItem("userName", fullName);
 
-        return { userId, userEmail };
+        return { userId, userEmail, fullName };
     },
 
     async register(fullName: string, email: string, password: string) {
-        const res = await api.post("/auth/register", {
-            fullName,
-            email,
-            password
-        });
+        const res = await api.post("/auth/register", { fullName, email, password });
 
-        const { token, userId, email: userEmail } = res.data;
+        const { token, userId, email: userEmail, fullName: name } = res.data;
 
+        // Guarda sesion para evitar que el usuario se registre y tenga que volver a loguearse.
         localStorage.setItem("token", token);
+        localStorage.setItem("userName", name);
 
-        return { userId, userEmail };
+        return { userId, userEmail, fullName: name };
     },
 
     logout() {
         localStorage.removeItem("token");
-    }
-}
+        localStorage.removeItem("userName");
+    },
+};
